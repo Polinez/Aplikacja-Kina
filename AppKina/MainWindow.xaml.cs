fc8e2aa8ab7ed0779178feda9d572fe7f77c3482
Shizo15 <damianszymczyk153@gmail.com>
2024-10-27 19:35:56 +0100

    feat: added main page and main window buttons

--- AppKina/MainWindow.xaml.cs
+++ AppKina/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AppKina
 {
@@ -19,6 +10,27 @@ namespace AppKina
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Logowanie_Click(object sender, RoutedEventArgs e)
+        {
+            Logowanie logowanie = new Logowanie();
+            logowanie.Show();
+
+        }
+
+        private void Rejestracja_Click(object sender, RoutedEventArgs e)
+        {
+            Rejestracja rejestracja = new Rejestracja();
+            rejestracja.Show();
+        }
+
+        private void Glowna_Click(object sender, RoutedEventArgs e)
+        {
+            Strona_glowna strona_Glowna = new Strona_glowna();
+
+            strona_Glowna.Show();
         }
     }
 }
