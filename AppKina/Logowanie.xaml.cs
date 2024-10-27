9d12b232028d0420c818eed109ebdebaec42d372
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-10-27 22:44:59 +0100

    feat: adding connections to buttons

--- AppKina/Logowanie.xaml.cs
+++ AppKina/Logowanie.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Path = System.IO.Path;
+﻿using System.Windows;
 
 namespace AppKina
 {
@@ -24,27 +10,12 @@ namespace AppKina
         public Logowanie()
         {
             InitializeComponent();
-            SetImageSource();
+
         }
 
-
-        private void SetImageSource()
+        private void buttom_zaloguj_Click(object sender, RoutedEventArgs e)
         {
-            // Ścieżka do folderu projektu (Repozytorium/AppKina)
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string projectDirectory = Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\.."));
 
-            // Połączenie ze ścieżką do folderu "elementy graficzne"
-            string imagePath = Path.Combine(projectDirectory, "elementyGraficzne", "KinoLogo.png");
-
-            if (File.Exists(imagePath))
-            {
-                logo.Source = new BitmapImage(new Uri(imagePath));
-            }
-            else
-            {
-                MessageBox.Show($"Obraz nie został znaleziony. ");
-            }
         }
     }
 
--- AppKina/Rejestracja.xaml.cs
+++ AppKina/Rejestracja.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Path = System.IO.Path;
+﻿using System.Windows;
+
 
 namespace AppKina
 {
@@ -24,28 +11,10 @@ namespace AppKina
         public Rejestracja()
         {
             InitializeComponent();
-            SetImageSource();
+
         }
 
 
-        private void SetImageSource()
-        {
 
-            // Ścieżka do folderu projektu (Repozytorium/AppKina)
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string projectDirectory = Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\.."));
-
-            // Połączenie ze ścieżką do folderu "elementy graficzne"
-            string imagePath = Path.Combine(projectDirectory, "elementyGraficzne", "KinoLogo.png");
-
-            if (File.Exists(imagePath))
-            {
-                logo.Source = new BitmapImage(new Uri(imagePath));
-            }
-            else
-            {
-                MessageBox.Show($"Obraz nie został znaleziony. ");
-            }
-        }
     }
 }
