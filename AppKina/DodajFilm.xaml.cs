eef8e0f057ea732d0559abc64f93d82e27236ee5
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-10-28 22:47:39 +0100

    Feat: site of adding film

---
+++ AppKina/DodajFilm.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace AppKina
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DodajFilm.xaml
+    /// </summary>
+    public partial class DodajFilm : Window
+    {
+        public DodajFilm()
+        {
+            InitializeComponent();
+        }
+
+        private void WyborZjd_Click(object sender, RoutedEventArgs e)
+        {
+            // Tworzenie nowego obiektu OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Ustawienia filtru plików na JPEG i PNG
+            openFileDialog.Filter = "Image Files (*.jpeg;*.jpg;*.png)|*.jpeg;*.jpg;*.png";
+
+            // Otwieranie okna dialogowego
+            bool? result = openFileDialog.ShowDialog();
+
+            // Sprawdzenie, czy użytkownik wybrał plik
+            if (result == true)
+            {
+                // Ścieżka do wybranego pliku
+                string selectedFilePath = openFileDialog.FileName;
+
+                // Możesz teraz wykorzystać wybraną ścieżkę, np. wyświetlić w TextBlocku
+                // lub załadować obraz do Image itp.
+                MessageBox.Show("Wybrano plik: " + selectedFilePath); // Dla przykładu, wyświetlenie ścieżki w oknie
+            }
+        }
+    }
+}
