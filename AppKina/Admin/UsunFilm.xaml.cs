44a453e2cd2e4499a3d1df1ba0ebb4d3c249404f
ankka812 <anna.walus@edu.uekat.pl>
2024-11-13 18:06:33 +0100

    feat: add and delete film- admin page

---
+++ AppKina/Admin/UsunFilm.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AppKina.Admin
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UsunFilm.xaml
+    /// </summary>
+    public partial class UsunFilm : Window
+    {
+        public UsunFilm()
+        {
+            InitializeComponent();
+        }
+
+        private void anuluj_click(object sender, RoutedEventArgs e)
+        {
+            GlownaStronaPracownika glownaStronaPracownika = new GlownaStronaPracownika();
+            glownaStronaPracownika.Show();
+
+            this.Close();
+        }
+    }
+}
