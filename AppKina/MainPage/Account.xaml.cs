368e54b947668e28c0b7d0e2d8778ad377eb869d
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-11-17 11:48:22 +0100

    feat: creating MyAccount Ui

---
+++ AppKina/MainPage/Account.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppKina.MainPage
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Account.xaml
+    /// </summary>
+    public partial class Account : Window
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        private void StronaGlowna_Click(object sender, RoutedEventArgs e)
+        {
+           Strona_glowna strona_Glowna = new Strona_glowna();
+            strona_Glowna.Show();
+            this.Close();
+        }
+
+        private void Rezerwuj_click(object sender, RoutedEventArgs e)
+        {
+            Zarezerwuj zarezerwuj = new Zarezerwuj();
+            zarezerwuj.Show();
+            this.Close();
+        }
+
+        private void MojeRezerwacje_click(object sender, RoutedEventArgs e)
+        {
+            MojeRezerwacje mojeRezerwacje = new MojeRezerwacje();
+            mojeRezerwacje.Show();
+            this.Close();
+        }
+
+        private void MojeKonto_click(object sender, RoutedEventArgs e)
+        {
+            //nic ma sie nie dziac
+        }
+
+        private void ZmienHaslo_click(object sender, RoutedEventArgs e)
+        {
+            //function that changes password
+        }
+
+        private void Wyloguj_click(object sender, RoutedEventArgs e)
+        {
+            LogowanieRejestracja logowanieRejestracja = new LogowanieRejestracja();
+            logowanieRejestracja.Show();
+            this.Close();
+        }
+    }
+}
