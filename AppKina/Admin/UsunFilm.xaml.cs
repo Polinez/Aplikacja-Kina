4322c9172de25639af72f07fe5c547537d5f188b
ankka812 <anna.walus@edu.uekat.pl>
2024-11-26 20:19:53 +0100

    feat: delete film function

--- AppKina/Admin/UsunFilm.xaml.cs
+++ AppKina/Admin/UsunFilm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Data.Sqlite;
+using System.Windows;
 
 namespace AppKina.Admin
 {
@@ -7,9 +8,11 @@ namespace AppKina.Admin
     /// </summary>
     public partial class UsunFilm : Window
     {
+        private const string SciezkaDoBazy = @"Data Source=KinoDB.db";
         public UsunFilm()
         {
             InitializeComponent();
+            ZaladujFilmy();
         }
 
         private void anuluj_click(object sender, RoutedEventArgs e)
@@ -18,6 +21,66 @@ namespace AppKina.Admin
             glownaStronaPracownika.Show();
 
             this.Close();
+        }
+
+        private void click_usun_film(object sender, RoutedEventArgs e)
+        {
+            if (FilmListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Wybierz film do usunięcia.");
+                return;
+            }
+            string wybranyFilm = FilmListBox.SelectedItem.ToString();
+            try
+            {
+                using (var polaczenie = new SqliteConnection(SciezkaDoBazy))
+                {
+                    polaczenie.Open();
+                    string zapytanie = "DELETE FROM Movies WHERE Title = @Title";
+                    using (var komenda = new SqliteCommand(zapytanie, polaczenie))
+                    {
+                        komenda.Parameters.AddWithValue("@Title", wybranyFilm);
+                        komenda.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show($"Film {wybranyFilm} zostal usunięty.");
+                ZaladujFilmy();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ZaladujFilmy()
+        {
+            FilmListBox.Items.Clear();
+            try
+            {
+                using (var polaczenie = new SqliteConnection(SciezkaDoBazy))
+                {
+                    polaczenie.Open();
+                    string zapytanie = "SELECT Title FROM Movies";
+                    using (var komenda = new SqliteCommand(zapytanie, polaczenie))
+                    using (var reader = komenda.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string nazwaFilmu = reader.GetString(0);
+                            FilmListBox.Items.Add(nazwaFilmu);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FilmListBox_wybor(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            usunFilm.IsEnabled = FilmListBox.SelectedItem != null;
         }
     }
 }
